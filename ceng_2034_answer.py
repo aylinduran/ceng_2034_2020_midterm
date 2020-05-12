7a0bd15473c1704a59c1d27034585027e3ff4e22
aylinduran <61870932+aylinduran@users.noreply.github.com>
2020-05-12 15:41:05 +0300

    Update ceng_2034_answer.py

--- ceng_2034_answer.py
+++ ceng_2034_answer.py
@@ -1,63 +1,44 @@
 #!/usr/bin/python3
 import os
-import sys
+import os,sys
 import urllib.request
+import requests
 import threading
+import multiprocessing
+
 from multiprocessing import pool
 
-Cpucount = (os.cpu_count())
-Loadavg = (os.getloadavg())
-
-
-def PrintPID():
-    print("Your PID is  ", os.getpid())
-
-
+Cpucount=(os.cpu_count())
+Loadavg=(os.getloadavg())
+def PrintPID ():
+   print("Your PID is  " , os.getpid())
 PrintPID()
 
-
-def LoadAvg():
-    print("My operating system is ", sys.platform)
-    print("MY loadavg is ", os.getloadavg())
-
-
+def LoadAvg ():
+   print("My operating system is " , sys.platform)
+   print ("MY loadavg is ", os.getloadavg())
 LoadAvg()
 
-
-def Calculate():
-    print("Cpu core count is ", Cpucount)
-    print("5 min loadavg is ", Loadavg[1])
-    a = float(Cpucount - Loadavg[1])
-    if a < 1:
-        print(sys.exit())
-    else:
-        print("Your cpu core count - 5min load avg > 1 ")
-
-
+def Calculate ():
+   print("Cpu core count is " , Cpucount)
+   print("5 min loadavg is " , Loadavg[1])
+   a=float(Cpucount-Loadavg[1])
+   if a<1:
+       print(sys.exit())
+   else :
+       print("Your cpu core count - 5min load avg > 1 ")
 Calculate()
 
-site = ["https://api.github.com", "http://bilgisayar.mu.edu.tr/", "https://www.python.org/",
-        "http://akrepnalan.com/ceng2034", "https://github.com/caesarsalad/wow"]
 
+def check(url):   
+    isworking = requests.get(url)
+    print(isworking.status_code, " " , url)
 
-def check(url):
-    for i in range(5):
-        print("Checking...", url[i], os.getpid())
-        if (urllib.request.urlopen(url[i]).getcode() == 200):
-            print(url[i], " is valid")
-
-        else:
-            print("There is a error , ", url[i], " is not valid !")
-
-
-check(site)
-
-'''
-thread1=threading.Thread(target=check, args=("https://www.python.org/" ,))
+thread1=threading.Thread(target=check, args=("https://www.python.org/",))
 thread2=threading.Thread(target=check, args=("http://bilgisayar.mu.edu.tr/",))
-thread3=threading.Thread(target=check, args=("https://api.github.com ",))
+thread3=threading.Thread(target=check, args=("https://api.github.com",))
 thread4=threading.Thread(target=check, args=("http://akrepnalan.com/ceng2034",))
-thread5=threading.Thread(target=check, args=("github.com/caesarsalad/wow" ,))
+thread5=threading.Thread(target=check, args=("https://github.com/caesarsalad/wow",))
 
 thread1.start()
 thread2.start()
@@ -71,10 +52,4 @@ thread3.join()
 thread4.join() 
 thread5.join()
 print('the endd')
-
-  with Pool(5) as p:
-    print(p.map(check, ["https://api.github.com", "http://bilgisayar.mu.edu.tr/", "https://www.python.org/",
-        "http://akrepnalan.com/ceng2034", "https://github.com/caesarsalad/wow"])) '''
-
-
 
